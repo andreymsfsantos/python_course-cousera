52ede31f69615464a1bdc1fe3f1dfe21ba03f786
andreymsfsantos <62684260+andreymsfsantos@users.noreply.github.com>
2026-07-22 11:10:15 -0300

    Add files via upload

---
+++ ex05.py
@@ -0,0 +1,10 @@
+# text = "X-DSPAM-Confidence:    0.8475"
+
+str = "X-DSPAM-Confidence:    0.8475"
+ipos = str.find(":")
+# print(ipos)
+piece = str[ipos+2:]
+# print(piece)
+value = float(piece)
+print(value)
+#print(value=42.0)
